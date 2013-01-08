2325750a10710ad8ff5ad558ed0e6cce18405c54
Cv Solutions <cvsolutions@me.com>
2013-01-09 00:49:43 +0100

    update 2

---
+++ application/controllers/LinksController.php
@@ -0,0 +1,41 @@
+<?php
+
+class LinksController extends Zend_Controller_Action
+{
+
+    public function preDispatch() {
+
+        /* disable Layout */
+        $this->_helper->layout()->disableLayout();
+        $this->_helper->viewRenderer->setNoRender( true );
+    }
+
+    public function init()
+    {
+        /* Initialize action controller here */
+    }
+
+    public function indexAction()
+    {
+        $url = $this->_getParam('url', 0);
+        $location = $this->_getParam('location', 0);
+
+        if($this->getRequest())
+        {
+            if(isset($_SESSION['ADS_Mapping']))
+            {
+                $this->redirect($url);
+                exit();
+            } else {
+                $_SESSION['ADS_Mapping'] = rand();
+                $Links = new Application_Model_DbTable_Links();
+                $Links->Mapping_Url($url, $location);
+                $this->redirect($url);
+                exit();
+            }
+        }
+    }
+
+
+}
+
