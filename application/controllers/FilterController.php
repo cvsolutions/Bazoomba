72f45bfdec7d5eba2e58920d9f3ed06dd4d4129e
Cv Solutions <cvsolutions@me.com>
2013-01-26 13:01:45 +0100

    update project
    add layout category, add rance price category, fixed bux

--- application/controllers/FilterController.php
+++ application/controllers/FilterController.php
@@ -30,7 +30,8 @@ class FilterController extends Zend_Controller_Action
      *
      * @return mixed Value.
      */
-    public function init() {
+    public function init()
+    {
 
         /* Initialize action controller here */
         $this->params = Plugin_Common::getParams();
@@ -42,21 +43,17 @@ class FilterController extends Zend_Controller_Action
         $identity = $auth->getStorage()->read();
         $this->view->identity = $identity;
     }
-    
+
     /**
-     * Creo un array di valori
-     * con i prezzi della ricerca
-     * 
-     * @param unknown_type $shop Array DB List
-     * @return multitype:
+     * @param array $shop
+     *
+     * @return array
      */
     private function slider_range($shop = array())
     {
-        if(is_array($shop))
-        {
+        if (is_array($shop)) {
             // print_r($shop);
-            foreach ($shop as $row)
-            {
+            foreach ($shop as $row) {
                 $price = sprintf('%d', $row['price']);
                 $item[] = $price;
             }
@@ -71,7 +68,8 @@ class FilterController extends Zend_Controller_Action
      *
      * @return mixed Value.
      */
-    public function indexAction() {
+    public function indexAction()
+    {
 
         /* creo il form per la ricerca veloce */
         $form = new Application_Form_Filter();
@@ -86,7 +84,7 @@ class FilterController extends Zend_Controller_Action
 
         /* le categorie attive */
         $category = new Application_Model_DbTable_Category();
-        $this->view->category = $category->Parent_With_Category( 0 );
+        $this->view->category = $category->Parent_With_Category(0);
     }
 
     /**
@@ -97,31 +95,32 @@ class FilterController extends Zend_Controller_Action
      *
      * @return mixed Value.
      */
-    public function regionAction() {
+    public function regionAction()
+    {
 
         /* recupero i parametri */
-        $id = $this->_getParam( 'item', 0 );
-        $ads = $this->_getParam( 'ads', 0 );
-        $user = $this->_getParam( 'user', 0 );
+        $id = $this->_getParam('item', 0);
+        $ads = $this->_getParam('ads', 0);
+        $user = $this->_getParam('user', 0);
 
         $region = new Application_Model_DbTable_Region();
 
         /* tutte le informazioni della regione selezionata */
-        $this->view->region = $region->getRegionInfo( $id );
+        $this->view->region = $region->getRegionInfo($id);
 
         /* le altre regioni escluso quella selezionata*/
-        $this->view->other = $region->Other_Region( $id );
+        $this->view->other = $region->Other_Region($id);
 
         /* tutte le province della regione*/
         $provinces = new Application_Model_DbTable_Provinces();
-        $this->view->provinces = $provinces->Parent_Provinces( $id );
+        $this->view->provinces = $provinces->Parent_Provinces($id);
 
         /**
          * creo l'array con la lista degli annunci
          * mostro a video+array()
          */
         $shop = new Application_Model_DbTable_Shop();
-        $fullShop = $shop->fullShopFilter( array( 'type' => 'region', 'id' => $id, 'ads' => $ads, 'user' => $user ) );
+        $fullShop = $shop->fullShopFilter(array('type' => 'region', 'id' => $id, 'ads' => $ads, 'user' => $user));
         $this->view->list = $fullShop;
         $this->view->range = $this->slider_range($fullShop);
         $this->view->type_ads = $this->params->type_ads->toArray();
@@ -137,30 +136,36 @@ class FilterController extends Zend_Controller_Action
      *
      * @return mixed Value.
      */
-    public function categoryAction() {
+    public function categoryAction()
+    {
 
         /* recupero i parametri */
-        $id = $this->_getParam( 'item', 0 );
-        $ads = $this->_getParam( 'ads', 0 );
-        $user = $this->_getParam( 'user', 0 );
+        $id = $this->_getParam('item', 0);
+        $ads = $this->_getParam('ads', 0);
+        $user = $this->_getParam('user', 0);
 
         $category = new Application_Model_DbTable_Category();
 
         /* tutte le info della categoria selezionata */
-        $this->view->category = $category->getCategoryInfo( $id );
+        $this->view->category = $category->getCategoryInfo($id);
 
         /* le sottocategorie della categoria madre*/
-        $this->view->parent = $category->Parent_With_Category( $id );
+        $this->view->parent = $category->Parent_With_Category($id);
 
         /* le altre categorie escluso la categoria selezionata*/
-        $this->view->other = $category->Other_Category( $id );
+        $this->view->other = $category->Other_Category($id);
 
         /**
          * creo l'array con la lista degli annunci
          * mostro a video+array()
          */
         $shop = new Application_Model_DbTable_Shop();
-        $this->view->list = $shop->fullShopFilter( array( 'type' => 'category', 'id' => $id, 'ads' => $ads, 'user' => $user ) );
+        $fullShop = $shop->fullShopFilter(
+            array('type' => 'category', 'id' => $id, 'ads' => $ads, 'user' => $user)
+        );
+        
+        $this->view->list = $fullShop;
+        $this->view->range = $this->slider_range($fullShop);
         $this->view->type_ads = $this->params->type_ads->toArray();
         $this->view->type_user = $this->params->type_user->toArray();
         $this->view->notfound = $this->params->label_not_found;
@@ -175,34 +180,35 @@ class FilterController extends Zend_Controller_Action
      *
      * @return mixed Value.
      */
-    public function provinceAction() {
+    public function provinceAction()
+    {
 
         /* recupero i parametri */
-        $id = $this->_getParam( 'item', 0 );
-        $parent = $this->_getParam( 'parent', 0 );
+        $id = $this->_getParam('item', 0);
+        $parent = $this->_getParam('parent', 0);
 
         $region = new Application_Model_DbTable_Region();
 
         /* informazioni sulla regione */
-        $this->view->region = $region->getRegionInfo( $parent );
+        $this->view->region = $region->getRegionInfo($parent);
 
         /* le alre regioni escluso quella selezionata*/
-        $this->view->other = $region->Other_Region( $parent );
+        $this->view->other = $region->Other_Region($parent);
 
         $provinces = new Application_Model_DbTable_Provinces();
 
         /* informazioni sulla provincia*/
-        $this->view->province = $provinces->getProvinceInfo( $id );
+        $this->view->province = $provinces->getProvinceInfo($id);
 
         /* le altre province della regione*/
-        $this->view->provinces = $provinces->Parent_Provinces( $parent );
+        $this->view->provinces = $provinces->Parent_Provinces($parent);
 
         /**
          * creo l'array con la lista degli annunci
          * mostro a video+array()
          */
         $shop = new Application_Model_DbTable_Shop();
-        $this->view->list = $shop->fullShopFilter( array( 'type' => 'province', 'id' => $id ) );
+        $this->view->list = $shop->fullShopFilter(array('type' => 'province', 'id' => $id));
         $this->view->type_ads = $this->params->type_ads->toArray();
         $this->view->type_user = $this->params->type_user->toArray();
         $this->view->notfound = $this->params->label_not_found;
@@ -217,30 +223,31 @@ class FilterController extends Zend_Controller_Action
      *
      * @return mixed Value.
      */
-    public function subcategoryAction() {
+    public function subcategoryAction()
+    {
 
         /* recupero i parametri */
-        $id = $this->_getParam( 'item', 0 );
-        $parent = $this->_getParam( 'parent', 0 );
+        $id = $this->_getParam('item', 0);
+        $parent = $this->_getParam('parent', 0);
 
         $category = new Application_Model_DbTable_Category();
 
         /* informazioni sulla categoria madre */
-        $this->view->category = $category->getCategoryInfo( $id );
-        $this->view->sub = $category->getCategoryInfo( $parent );
+        $this->view->category = $category->getCategoryInfo($id);
+        $this->view->sub = $category->getCategoryInfo($parent);
 
         /* altre sottocategorie della categoria madre */
-        $this->view->parent = $category->Parent_With_Category( $id );
+        $this->view->parent = $category->Parent_With_Category($id);
 
         /* le altre categorie escluso quella selezionata */
-        $this->view->other = $category->Other_Category( $id );
+        $this->view->other = $category->Other_Category($id);
 
         /**
          * creo l'array con la lista degli annunci
          * mostro a video+array()
          */
         $shop = new Application_Model_DbTable_Shop();
-        $this->view->list = $shop->fullShopFilter( array( 'type' => 'sub_category', 'id' => $parent ) );
+        $this->view->list = $shop->fullShopFilter(array('type' => 'sub_category', 'id' => $parent));
         $this->view->type_ads = $this->params->type_ads->toArray();
         $this->view->type_user = $this->params->type_user->toArray();
         $this->view->notfound = $this->params->label_not_found;
@@ -256,14 +263,15 @@ class FilterController extends Zend_Controller_Action
      *
      * @return mixed Value.
      */
-    public function searchAction() {
+    public function searchAction()
+    {
 
         /* recupero i parametri */
-        $q = $this->_getParam( 'q', 0 );
-        $category = $this->_getParam( 'category', 0 );
-        $region = $this->_getParam( 'region', 0 );
-        $type = $this->_getParam( 'type', 0 );
-        $ads = $this->_getParam( 'ads', 0 );
+        $q = $this->_getParam('q', 0);
+        $category = $this->_getParam('category', 0);
+        $region = $this->_getParam('region', 0);
+        $type = $this->_getParam('type', 0);
+        $ads = $this->_getParam('ads', 0);
 
         /**
          * scambio la modalità di ricerca
@@ -273,40 +281,42 @@ class FilterController extends Zend_Controller_Action
          * global-> applico i filtri di ricerca che possono essere
          * concatenati tra di loro / category / region / input
          */
-        switch ( $type ) {
+        switch ($type) {
 
-        case 'autocomplete':
-            $this->_redirect( sprintf( '/shop/ads/show/%d', $ads ) );
-            break;
+            case 'autocomplete':
+                $this->_redirect(sprintf('/shop/ads/show/%d', $ads));
+                break;
 
-        case 'global':
-            if ( $this->getRequest()->getParams() ) {
-                $shop = new Application_Model_DbTable_Shop();
-                $result = $shop->fullShopFilter( array(
-                        'type' => $type,
-                        'category' => $category,
-                        'region' => $region,
-                        'q' => $q
-                    ) );
+            case 'global':
+                if ($this->getRequest()->getParams()) {
+                    $shop = new Application_Model_DbTable_Shop();
+                    $result = $shop->fullShopFilter(
+                        array(
+                             'type'     => $type,
+                             'category' => $category,
+                             'region'   => $region,
+                             'q'        => $q
+                        )
+                    );
 
-                /* Zend_Paginator */
-                $page = $this->_getParam( 'page', 1 );
-                $paginator = Zend_Paginator::factory( $result );
-                $paginator->setItemCountPerPage( 2 );
-                $paginator->setCurrentPageNumber( $page );
+                    /* Zend_Paginator */
+                    $page = $this->_getParam('page', 1);
+                    $paginator = Zend_Paginator::factory($result);
+                    $paginator->setItemCountPerPage(2);
+                    $paginator->setCurrentPageNumber($page);
 
-                /**
-                 * lista dei risultati
-                 * totale degli annunci
-                 * array()
-                 */
-                $this->view->list = $paginator;
-                $this->view->total_list = $paginator->getTotalItemCount();
-                $this->view->type_ads = $this->params->type_ads->toArray();
-                $this->view->type_user = $this->params->type_user->toArray();
-                $this->view->notfound = $this->params->label_not_found;
-            }
-            break;
+                    /**
+                     * lista dei risultati
+                     * totale degli annunci
+                     * array()
+                     */
+                    $this->view->list = $paginator;
+                    $this->view->total_list = $paginator->getTotalItemCount();
+                    $this->view->type_ads = $this->params->type_ads->toArray();
+                    $this->view->type_user = $this->params->type_user->toArray();
+                    $this->view->notfound = $this->params->label_not_found;
+                }
+                break;
         }
     }
 
